479fa8b06c5cb94330c5791e31009ad987080a80
cataclysmnik <sagniksinghaishere@gmail.com>
2025-06-27 15:50:17 +0530

    Setup the page-routes

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react';
+
+const Gallery = () => {
+  return (
+    <div>Gallery</div>
+  )
+}
+
+export default Gallery
---
+++ src/app/join/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react';
+
+const Join = () => {
+  return (
+    <div>Join</div>
+  )
+}
+
+export default Join
