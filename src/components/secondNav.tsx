02381a833f2cff29d313820979d280cba8ae9609
cataclysmnik <sagniksinghaishere@gmail.com>
2025-09-04 22:29:28 +0530

    Added borders to the desktop navbar

--- src/components/secondNav.tsx
+++ src/components/secondNav.tsx
@@ -25,7 +25,7 @@ export default function SecondNav() {
   ];
 
   return (
-    <nav className='fixed top-4 left-1/2 transform -translate-x-1/2 w-[calc(100%-10rem)] hidden lg:flex items-center justify-between px-8 py-4 bg-black/10 backdrop-blur-xl rounded-2xl z-[4000]'>
+    <nav className='fixed top-4 left-1/2 transform -translate-x-1/2 w-[calc(100%-10rem)] border-2 border-white/10 hidden lg:flex items-center justify-between px-8 py-4 bg-black/10 backdrop-blur-xl rounded-2xl z-[4000]'>
       <Image src={`/logo.svg`} width={45} height={45} alt="logo" />
       <div>
         <ul className='flex gap-7 font-semibold'>
@@ -33,9 +33,8 @@ export default function SecondNav() {
             <li key={index} className='text-[20px]'>
               <Link
                 href={element.href}
-                className={`hover:text-gray-300 text-white pb-1 border-b-2 transition-all ${
-                  pathname === element.href ? 'border-white hover:border-gray-500' : 'border-transparent'
-                }`}
+                className={`hover:text-gray-300 text-white pb-1 border-b-2 transition-all ${pathname === element.href ? 'border-white hover:border-gray-500' : 'border-transparent'
+                  }`}
               >
                 {element.label}
               </Link>
